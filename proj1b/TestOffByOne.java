974e376ef8823502d64dc177ece720f470883d24
Roy Lee <Li-jinquan@outlook.com>
2019-03-19 15:12:00 +0800

    Test proj ----modified 7!

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -2,17 +2,17 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestOffByOne {
-    static CharacterComparator obo = new OffByOne();
+    static CharacterComparator offByOne = new OffByOne();
     // Your tests go here.
     @Test
     public void test_equalChars() {
-        assertTrue(obo.equalChars('a', 'b'));
-        assertTrue(obo.equalChars('b', 'a'));
-        assertTrue(obo.equalChars('&', '%'));
+        assertTrue(offByOne.equalChars('a', 'b'));
+        assertTrue(offByOne.equalChars('b', 'a'));
+        assertTrue(offByOne.equalChars('&', '%'));
 
-        assertFalse(obo.equalChars('a', 'c'));
-        assertFalse(obo.equalChars('c', 'a'));
-        assertFalse(obo.equalChars('a', 'a'));
-        assertFalse(obo.equalChars('A', 'a'));
+        assertFalse(offByOne.equalChars('a', 'c'));
+        assertFalse(offByOne.equalChars('c', 'a'));
+        assertFalse(offByOne.equalChars('a', 'a'));
+        assertFalse(offByOne.equalChars('A', 'a'));
     }
 }
