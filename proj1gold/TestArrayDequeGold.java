e32a8d2da7eab26c2b7b169721a7a148dd72adb7
Roy Lee <Li-jinquan@outlook.com>
2019-03-22 20:53:01 +0800

    proj1gold test

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -10,39 +10,41 @@ public class TestArrayDequeGold {
     StudentArrayDeque<Integer> stud1 = new StudentArrayDeque<>();
     ArrayDequeSolution<Integer> solu = new ArrayDequeSolution<>();
 
-    @Test  //StudentArrayDeque and ad
-    public void testAddLast() {
-        // initialize an Deque
-        int a = -10;
-        int b = 10;
-        int temp;
-        int temp1;
-        for (Integer i = 0; i < 10; i += 1) {
-            int num = StdRandom.uniform(a, b) ;
-            stud1.addFirst(num);
-            solu.addFirst(num);
+    int a = -10;
+    int b = 10;
+    int loopVal = 10;
+    int temp;
+    int temp1;
+
+    @Test
+    public void RandomizedTesting() {
+        for (int i = 0; i < loopVal; i++) {
+            int item = StdRandom.uniform(10);
+            stud1.addLast(item);
+            solu.addLast(item);
         }
 
-        for (int i = 0; i < 10; i += 1) {
-            if (!stud1.isEmpty() & !solu.isEmpty()) {
-                assertEquals(stud1.removeFirst(), solu.removeFirst());
-            }
+        String msg = helper(stud1);
+        for (int i = 0; i < loopVal; i++) {
+            Integer removed = stud1.removeLast();
+            Integer removed1 = solu.removeLast();
+            msg = helper1(msg);
+            assertEquals(msg, removed1, removed);
         }
-
-        for (Integer i = 0; i < 10; i += 1) {
-            int num = StdRandom.uniform(a, b) ;
-            stud1.addLast(num);
-            solu.addLast(num);
-        }
-
-        for (int i = 0; i < 10; i += 1) {
-            if (!stud1.isEmpty() & !solu.isEmpty()) {
-                temp = solu.removeLast();
-                temp1 = stud1.removeLast();
-                assertEquals(temp, temp1);
-                //assertEquals(solu.removeLast(), stud1.removeLast());
-            }
-        }
-
     }
+
+    private String helper(StudentArrayDeque <Integer> array) {
+        String msg = "";
+        for (int i = 0; i < loopVal; i ++) {
+            msg = msg + "addFirst(" + array.get(i).toString() + ")\n";
+        }
+
+        return msg;
+    }
+
+    private String helper1(String msg) {
+        return msg + "removeLast()\n";
+    }
+
+
 }
