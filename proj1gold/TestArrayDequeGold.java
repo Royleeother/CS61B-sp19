6eaca9fa5cf6d807bdd6f5f704f0994057d520a9
Roy Lee <Li-jinquan@outlook.com>
2019-03-22 20:19:55 +0800

    proj1gold test for task1!

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -15,29 +15,34 @@ public class TestArrayDequeGold {
         // initialize an Deque
         int a = -10;
         int b = 10;
-        Integer tempt;
-        Integer tempt1;
-        for (Integer i = 0; i < 5; i += 1) {
-            //int numberBetweenZeroAndOne = StdRandom.uniform(a, b) ;
-            stud1.addFirst(i);
-            solu.addFirst(i);
+        int temp;
+        int temp1;
+        for (Integer i = 0; i < 10; i += 1) {
+            int num = StdRandom.uniform(a, b) ;
+            stud1.addFirst(num);
+            solu.addFirst(num);
         }
 
-        for (int i = 0; i < 5; i += 1) {
+        for (int i = 0; i < 10; i += 1) {
             if (!stud1.isEmpty() & !solu.isEmpty()) {
-                Integer num1 = StdRandom.uniform(a, b);
-                Integer num2 = StdRandom.uniform(a, b);
-
-                stud1.addFirst(num1);
-                solu.addFirst(num1);
-
                 assertEquals(stud1.removeFirst(), solu.removeFirst());
-
-                stud1.addLast(num2);
-                solu.addLast(num2);
-
-                assertEquals(solu.removeLast(), stud1.removeLast());
             }
         }
+
+        for (Integer i = 0; i < 10; i += 1) {
+            int num = StdRandom.uniform(a, b) ;
+            stud1.addLast(num);
+            solu.addLast(num);
+        }
+
+        for (int i = 0; i < 10; i += 1) {
+            if (!stud1.isEmpty() & !solu.isEmpty()) {
+                temp = solu.removeLast();
+                temp1 = stud1.removeLast();
+                assertEquals(temp, temp1);
+                //assertEquals(solu.removeLast(), stud1.removeLast());
+            }
+        }
+
     }
 }
