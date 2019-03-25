da5ebb54bc089b57c84d0236106583cff8232e66
Roy Lee <Li-jinquan@outlook.com>
2019-03-25 18:16:17 +0800

    first try

--- hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
+++ hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 //TODO: Make sure to add the override tag for all overridden methods
 //TODO: Make sure to make this class implement BoundedQueue<T>
 
-public class ArrayRingBuffer<T>  {
+public class ArrayRingBuffer<T> implements BoundedQueue<T> {
     /* Index for the next dequeue or peek. */
     private int first;
     /* Index for the next enqueue. */
@@ -14,43 +14,82 @@ public class ArrayRingBuffer<T>  {
     private int fillCount;
     /* Array for storing the buffer data. */
     private T[] rb;
-
+    // record for capacity
+    int kp;
     /**
      * Create a new ArrayRingBuffer with the given capacity.
      */
     public ArrayRingBuffer(int capacity) {
         // TODO: Create new array with capacity elements.
         //       first, last, and fillCount should all be set to 0.
+        kp = capacity;
+        rb = (T[]) new Object[capacity];
+        first = capacity / 2;
+        last  = capacity / 2;
+        fillCount = 0;
+    }
+
+    @Override
+    public int capacity() {
+        return kp;
+    }
+    @Override
+    public int fillCount() {
+        return fillCount;
     }
 
     /**
      * Adds x to the end of the ring buffer. If there is no room, then
      * throw new RuntimeException("Ring buffer overflow").
      */
+    @Override
     public void enqueue(T x) {
         // TODO: Enqueue the item. Don't forget to increase fillCount and update
         //       last.
-        return;
+        if (isFull()) {
+            throw new RuntimeException("Ring buffer overflow");
+        }
+        rb[last] = x;
+        last += 1;
+        fillCount += 1;
+        if (last == capacity()) {
+            last = 0;
+        }
     }
 
     /**
      * Dequeue oldest item in the ring buffer. If the buffer is empty, then
      * throw new RuntimeException("Ring buffer underflow").
      */
+    @Override
     public T dequeue() {
         // TODO: Dequeue the first item. Don't forget to decrease fillCount and
         //       update first.
-        return null;
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        T re =  (T) rb[first];
+        rb[first] = null;
+        first += 1;
+        fillCount -= 1;
+        if (first == capacity()) {
+            first = 0;
+        }
+        return re;
     }
 
     /**
      * Return oldest item, but don't remove it. If the buffer is empty, then
      * throw new RuntimeException("Ring buffer underflow").
      */
+    @Override
     public T peek() {
         // TODO: Return the first item. None of your instance variables should
         //       change.
-        return null;
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        return (T) rb[first];
     }
 
     // TODO: When you get to part 4, implement the needed code to support
--- hw1/es/datastructur/synthesizer/GuitarString.java
+++ hw1/es/datastructur/synthesizer/GuitarString.java
@@ -1,4 +1,6 @@
 package es.datastructur.synthesizer;
+import edu.princeton.cs.introcs.StdAudio;
+import java.nio.Buffer;
 
 //Note: This file will not compile until you complete task 1 (BoundedQueue).
 public class GuitarString {
@@ -16,6 +18,12 @@ public class GuitarString {
         //       cast the result of this division operation into an int. For
         //       better accuracy, use the Math.round() function before casting.
         //       Your buffer should be initially filled with zeros.
+        double capacity = SR / frequency;
+        int kp = (int) Math.round(capacity);
+        buffer = new ArrayRingBuffer<>(kp);
+        for (int i = 0; i < buffer.capacity(); i++) {
+            buffer.enqueue(0.0);
+        }
     }
 
 
@@ -27,6 +35,21 @@ public class GuitarString {
         //
         //       Make sure that your random numbers are different from each
         //       other.
+        // (int)(Math.random() * ((upperbound - lowerbound) + 1) + lowerbound);
+        int size = buffer.fillCount();
+        while (!buffer.isEmpty()) {
+            buffer.dequeue();
+        }
+        double res = 0;
+        double num = 0;
+        for (int i = 0; i < buffer.capacity(); i += 1) {
+            num = Math.random() - 0.5;
+            while (res == num) {
+                num = Math.random() - 0.5;
+            }
+            res = num;
+            buffer.enqueue(num);
+        }
     }
 
     /* Advance the simulation one time step by performing one iteration of
@@ -36,12 +59,15 @@ public class GuitarString {
         // TODO: Dequeue the front sample and enqueue a new sample that is
         //       the average of the two multiplied by the DECAY factor.
         //       Do not call StdAudio.play().
+        double front = buffer.dequeue();
+        double newDouble = (front + buffer.peek()) * 0.5 * DECAY;
+        buffer.enqueue(newDouble);
     }
 
     /* Return the double at the front of the buffer. */
     public double sample() {
         // TODO: Return the correct thing.
-        return 0;
+        return buffer.peek();
     }
 }
     // TODO: Remove all comments that say TODO when you're done.
