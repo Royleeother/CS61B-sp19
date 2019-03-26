df6c2065eba4e25fcb6f21d7dc5f90d4c9ce51b0
Roy Lee <Li-jinquan@outlook.com>
2019-03-26 15:18:23 +0800

    fixed Testiterator

--- hw1/es/datastructur/synthesizer/TestArrayRingBuffer.java
+++ hw1/es/datastructur/synthesizer/TestArrayRingBuffer.java
@@ -12,10 +12,17 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void testIterator() {
-        /*ArrayRingBuffer arb = new ArrayRingBuffer(10);
-        arb.enqueue(1);
-        arb.enqueue(2);*/
-
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer<>(10);
+        for (int i = 0; i < arb.capacity(); i += 1) {
+            arb.enqueue(i * 2);
+        }
+        Iterator iterator = arb.iterator();
+        //Iterator<Integer> in = new Iterator<>();
+        for (int x : arb) {
+            for (int y : arb) {
+                System.out.println("x: " + x + "; y: " + y);
+            }
+        }
         //Iterator<String> this_iter = new Iterable<>();
         //BoundedQueue<Integer> arb = new
     }
