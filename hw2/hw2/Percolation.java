f6fe413bed0470eb28180bba954687fac774eef0
Roy Lee <Li-jinquan@outlook.com>
2019-03-28 22:10:59 +0800

    First try Percolation

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,88 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    /**
+     * RULE:
+     *  -1 stands for block
+     *  0 or greater numbers stand for open
+     */
+    private int num; // number of open sites
+    public UnionFind grid; // N * N grid
+    private int NN; // N * N
 
+    // create N-by-N grid, with all sites initially blocked
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        grid  = new UnionFind(N * N);
+        NN = N;
+    }
+    // open the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        if (!validInput(row, col)) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        if (!isOpen(row, col)) {
+            int pos = xyTo1D(row, col);
+            grid.setParent(pos);
+            num += 1;
+        }
+    }
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        if (!validInput(row, col)) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        int pos = xyTo1D(row, col);
+        return (grid.parent(pos) >= 0);
+    }
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        if (!validInput(row, col)) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        if (row == 0) {
+            return true;
+        }
+        int present_pos = xyTo1D(row, col);
+        for (int i = 0; i < NN; i += 1) {
+            int top_pos = xyTo1D(0, i);
+            while (grid.connected(present_pos, top_pos)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    // number of open sites
+    public int numberOfOpenSites() {
+        return num;
+    }
+    // does the system percolate?
+    public boolean percolates() {
+        for (int i = 0; i < NN; i += 1) {
+            while (isFull(NN, i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // use for unit testing (not required, but keep this here for the autograder)
+    public static void main(String[] args) {
+        Percolation fillter = new Percolation(5);
+        fillter.open(3, 4);
+        //boolean haha =  fillter.isOpen(3, 4);
+        fillter.open(0, 4);
+        boolean haha = fillter.isFull(0, 4);
+    }
+
+    // convert (x,y) to num
+    private int xyTo1D(int r, int c) {
+        return (r * NN) + c;
+    }
+    // check whether input the correct row, col
+    private boolean validInput(int row, int col) {
+        return (row >= 0 && row <= NN - 1 && col >= 0 && col <= NN - 1);
+    }
 }
---
+++ hw2/hw2/UnionFind.java
@@ -0,0 +1,94 @@
+package hw2;
+public class UnionFind {
+
+    // TODO - Add instance variables?
+    //private int size[]; // size of one set
+    //private int id[]; // the component that store sets
+    private int[] id;
+    private int[] size;
+
+    /* Creates a UnionFind data structure holding n vertices. Initially, all
+       vertices are in disjoint sets. */
+    public UnionFind(int n) {
+        id = new int[n];
+        size = new int[n];
+        // initialize a component with sets separately
+        for (int i = 0; i < n; i += 1) {
+            //id[i] = i;
+            id[i] = -1;
+            size[i] = 1;
+        }
+    }
+
+    /* Throws an exception if v1 is not a valid index. */
+    private void validate(int vertex) {
+        if (vertex < 0 || vertex >= id.length) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /* Returns the size of the set v1 belongs to. */
+    public int sizeOf(int v1) {
+        return size[find(v1)];
+    }
+
+    /* Returns the parent of v1. If v1 is the root of a tree, returns the
+       negative size of the tree for which v1 is the root. */
+    public int parent(int v1) {
+        return id[v1];
+    }
+
+    // for grid open
+    public void setParent(int v1) {
+        id[v1] = 0; // 0 stands for open(default)
+    }
+
+    /* Returns true if nodes v1 and v2 are connected. */
+    public boolean connected(int v1, int v2) {
+        int p1 = parent(v1);
+        int p2 = parent(v2);
+        if (p1 == p2 && sizeOf(v1) != 1 && sizeOf(v2) != 1) {
+            return true;
+        } else if (p1 < 0 || p2 < 0) {
+            return false;
+        }
+        return connected(p1, p2);
+    }
+
+    /* Connects two elements v1 and v2 together. v1 and v2 can be any valid 
+       elements, and a union-by-size heuristic is used. If the sizes of the sets
+       are equal, tie break by connecting v1's root to v2's root. Unioning a 
+       vertex with itself or vertices that are already connected should not 
+       change the sets but may alter the internal structure of the data. */
+    public void union(int v1, int v2) {
+        if (connected(v1, v2)) {
+            return;
+
+        } else if (sizeOf(v1) > sizeOf(v2)) {
+            int big_parent = find(v1);
+            int small_dd   = find(v2);
+            id[small_dd]   = big_parent;
+            size[v1] += size[v2];
+            id[big_parent] = -1 * size[v1];
+
+        } else if (sizeOf(v1) <= sizeOf(v2)) {
+            int big_parent = find(v2);
+            int small_dd   = find(v1);
+            id[small_dd]   = big_parent;
+            size[v2] += size[v1];
+            id[big_parent] = -1 * size[v1];
+        }
+    }
+
+    /* Returns the root of the set V belongs to. Path-compression is employed
+       allowing for fast search-time. */
+    public int find(int vertex) {
+        validate(vertex);
+        int p = parent(vertex);
+        if (p < 0) {
+            return vertex;
+        }
+        return find(p);
+    }
+
+}
