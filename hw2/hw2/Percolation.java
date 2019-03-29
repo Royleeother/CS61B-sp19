6a53ca5020ecae60b706d9bdaa735f6f88aa3082
Roy Lee <Li-jinquan@outlook.com>
2019-03-29 15:02:14 +0800

    Percolation succeed in the visualizer

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -2,59 +2,87 @@ package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+// WOCAO
 public class Percolation {
     /**
      * RULE:
-     *  -1 stands for block
-     *  0 or greater numbers stand for open
+     *  false stands for block
+     *  true stands for open
      */
     private int num; // number of open sites
-    public UnionFind grid; // N * N grid
+    public boolean[][] grid; // N * N grid
+    WeightedQuickUnionUF WQUF;
     private int NN; // N * N
+    private int head;
+    private int bottom;
 
     // create N-by-N grid, with all sites initially blocked
     public Percolation(int N) {
         if (N <= 0) {
             throw new java.lang.IllegalArgumentException();
         }
-        grid  = new UnionFind(N * N);
-        NN = N;
+        // initialize all the girds with false value which means block
+        grid = new boolean[N][N];
+        // plus one virtual top and one virtual bottom
+        WQUF = new WeightedQuickUnionUF((N * N) + 2);
+        // not a site open yet
+        num = 0;
+        NN  = N;
+        // virtual sites
+        head = N * N;
+        bottom = (N * N) + 1;
     }
     // open the site (row, col) if it is not open already
     public void open(int row, int col) {
-        if (!validInput(row, col)) {
-            throw new java.lang.IllegalArgumentException();
-        }
+        validate(row, col);
+
         if (!isOpen(row, col)) {
-            int pos = xyTo1D(row, col);
-            grid.setParent(pos);
+            grid[row][col] = true;
             num += 1;
+        }
+        int current_pos = xyTo1D(row, col);
+
+        // logic problem, can not use else if expression
+        // top-gird's duty
+        if (row == 0) WQUF.union(current_pos, head);
+        // bottom-grid's duty
+        if (row == (NN - 1)) WQUF.union(current_pos, bottom);
+        // upper neighbour
+        if ((row + 1) < NN && isOpen(row + 1, col)) {
+            int upper = xyTo1D(row + 1, col);
+            WQUF.union(current_pos, upper);
+        }
+        // nether neighbour
+        if ((row - 1) >= 0 && isOpen(row - 1, col)) {
+            int nether = xyTo1D(row - 1, col);
+            WQUF.union(current_pos, nether);
+        }
+        // left neighbour
+        if ((col - 1) >= 0 && isOpen(row, col - 1)) {
+            int left = xyTo1D(row, col - 1);
+            WQUF.union(current_pos, left);
+        }
+        // right neighbour
+        if ((col + 1) < NN && isOpen(row, col + 1)) {
+            int right = xyTo1D(row, col + 1);
+            WQUF.union(current_pos, right);
         }
     }
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        if (!validInput(row, col)) {
-            throw new java.lang.IllegalArgumentException();
-        }
-        int pos = xyTo1D(row, col);
-        return (grid.parent(pos) >= 0);
+        validate(row, col);
+
+        // return grid[row][col] == true; bad style
+        return grid[row][col];
     }
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        if (!validInput(row, col)) {
-            throw new java.lang.IllegalArgumentException();
-        }
-        if (row == 0) {
-            return true;
-        }
-        int present_pos = xyTo1D(row, col);
-        for (int i = 0; i < NN; i += 1) {
-            int top_pos = xyTo1D(0, i);
-            while (grid.connected(present_pos, top_pos)) {
-                return true;
-            }
-        }
-        return false;
+        validate(row, col);
+        if (!isOpen(row, col)) return false;
+
+        int current_pos = xyTo1D(row, col);
+        // whether the grid connected to head
+        return WQUF.connected(head, current_pos);
     }
     // number of open sites
     public int numberOfOpenSites() {
@@ -62,21 +90,16 @@ public class Percolation {
     }
     // does the system percolate?
     public boolean percolates() {
-        for (int i = 0; i < NN; i += 1) {
-            while (isFull(NN, i)) {
-                return true;
-            }
-        }
-        return false;
+        return WQUF.connected(head, bottom);
     }
 
     // use for unit testing (not required, but keep this here for the autograder)
     public static void main(String[] args) {
         Percolation fillter = new Percolation(5);
-        fillter.open(3, 4);
-        //boolean haha =  fillter.isOpen(3, 4);
-        fillter.open(0, 4);
-        boolean haha = fillter.isFull(0, 4);
+        fillter.open(0, 2);
+        fillter.open(2, 2);
+        boolean haha = fillter.isFull(0, 2);
+        boolean enen = fillter.isFull(2, 2);
     }
 
     // convert (x,y) to num
@@ -84,7 +107,10 @@ public class Percolation {
         return (r * NN) + c;
     }
     // check whether input the correct row, col
-    private boolean validInput(int row, int col) {
-        return (row >= 0 && row <= NN - 1 && col >= 0 && col <= NN - 1);
+    private boolean validate(int row, int col) {
+        if (!(row >= 0 && row <= NN - 1 && col >= 0 && col <= NN - 1)) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        return true;
     }
 }
