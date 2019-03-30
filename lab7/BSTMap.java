e121e8673ab66f7079e07859a6eb495207b69b2c
Roy Lee <Li-jinquan@outlook.com>
2019-03-30 11:03:10 +0800

    fix put() bug, added print_increasing

--- lab7/BSTMap.java
+++ lab7/BSTMap.java
@@ -1,4 +1,4 @@
-
+import java.util.Set;
 import java.util.Iterator;
 
 public class BSTMap<K extends Comparable<K>, V> implements Map61B<K,V> {
@@ -66,17 +66,18 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K,V> {
         else return x.count;
     }
     /* Associates the specified value with the specified key in this map. */
-    private void put_helper(Node x, K key, V value) {
-        if (x == null) new Node(key, value, 0);
+    private Node put_helper(Node x, K key, V value) {
+        if (x == null) x = new Node(key, value, 0);
         int cmp = key.compareTo(x.key);
-        if (cmp < 0) put_helper(x.left, key, value);
-        else if (cmp > 0) put_helper(x.right, key, value);
+        if (cmp < 0) x.left = put_helper(x.left, key, value);
+        else if (cmp > 0) x.right = put_helper(x.right, key, value);
         x.count = size(x.left) + size(x.right) + 1;
+        return x;
     }
 
     @Override
     public void put(K key, V value) {
-        put_helper(root, key, value);
+        root = put_helper(root, key, value);
     }
     /* Returns a Set view of the keys contained in this map. */
     @Override
@@ -96,15 +97,44 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K,V> {
         throw new UnsupportedOperationException();
     }
 
-    private void print_helper(Node x) {
-        if (x == null) System.out.println(' ');
+    private void print_helper_decreasing(Node x) {
+        if (x == null) System.out.print("");
         else {
-            System.out.println(x.value);
-            print_helper(x.right);
-            print_helper(x.left);
+            System.out.print(x.value + " ");
+            print_helper_decreasing(x.right);
+            print_helper_decreasing(x.left);
         }
     }
+    private void print_helper_increasing(Node x) {
+        if (x.left == null) {
+            System.out.print(x.value + " ");
+            if (x.right != null){
+                print_helper_increasing(x.right);
+            }
+        }
+        else {
+            print_helper_increasing(x.left);
+            if (x.right != null){
+                print_helper_increasing(x.right);
+            }
+            System.out.print(x.value + " ");
+        }
+    }
+    public void printInOrder_decreasing() {
+        print_helper_decreasing(root);
+    }
     public void printInOrder() {
-        print_helper(root);
+        print_helper_increasing(root);
+    }
+
+    public static void main(String arg[]) {
+        BSTMap<String, Integer> a = new BSTMap<String, Integer>();
+        a.put("hi3", 3);
+        a.put("hi1", 1);
+        a.put("hi2", 2);
+        a.put("hi0", 0);
+        a.printInOrder_decreasing();
+        System.out.println(" ");
+        a.printInOrder();
     }
 }
