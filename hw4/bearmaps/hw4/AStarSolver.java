fc4270d5ab1c59d4bcbaf73140716ee396e7581a
Roy Lee <Li-jinquan@outlook.com>
2019-04-18 17:00:03 +0800

    implement 'disTo' in HashMap

--- hw4/bearmaps/hw4/AStarSolver.java
+++ hw4/bearmaps/hw4/AStarSolver.java
@@ -3,23 +3,24 @@ package bearmaps.hw4;
 import bearmaps.proj2ab.DoubleMapPQ;
 import edu.princeton.cs.algs4.Stopwatch;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
     private AStarGraph<Vertex> graph;
     private Vertex source;
     private Vertex target;
     private double timeout;
-    private int explored;
+    private int explored = 0;
     private double timer;
     private List<Vertex> solution;
     private double solutionWeight = 0;
     private SolverOutcome outcome;
     private DoubleMapPQ Fringe;
-    private int[] disTo;
+    private Map<Vertex, Double> disTo;
+    //private int[] disTo;
     //private LinkedList<Vertex> disTo;
     //private int[] edgeTo;
     //private ArrayList<Vertex> edgeTo;
@@ -33,17 +34,19 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
         this.target = end;
         this.timeout = timeout;
 
-        disTo = new int[(int)end + 1];
+        disTo = new HashMap<>();
+        //disTo = new int[(int)end + 1];
         //disTo = new LinkedList<>();
         //edgeTo = new int[(int)end + 1];
         //edgeTo = new ArrayList<>((int)end + 1);
         edgeTo = new HashMap<>();
 
-        disTo[0] = 0;
+        /*disTo[0] = 0;
         //disTo.add(source);
         for (int i = 1; i < disTo.length; i++) {
             disTo[i] = 99999999;
-        }
+        }*/
+        disTo.put(start, 0.0);
         // initialize solution (empty list)
         //solution = new ArrayList<>();
         solution = new LinkedList<>();
@@ -56,7 +59,8 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
     private void aStar(AStarGraph<Vertex> G, Vertex s) {
         sw = new Stopwatch();
         Fringe = new DoubleMapPQ();
-        int pior = disTo[(int)s] + (int)G.estimatedDistanceToGoal(s, this.target);
+        //int pior = disTo[(int)s] + (int)G.estimatedDistanceToGoal(s, this.target);
+        double pior = disTo.get(s) + G.estimatedDistanceToGoal(s, this.target);
         Fringe.add(s, pior);
 
         while (Fringe.size() != 0 && sw.elapsedTime() < timeout) {
@@ -77,23 +81,10 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
                     //solution.add(v);
                     v = edgeTo.get(v);
                 }
-                // reverse
-                /*int ss = solution.size();
-                List<Vertex> temp = new;
-                //int[] temp = new int[ss];
-                /*for (int i = 0; i < temp.length; i++) {
-                    temp[i] = (int) solution.get(ss - i);
-                }
-                for (int i = 0; i < solution.size() / 2; i++) {
-                    //temp[i] = solution.get(i);
-                    temp.add(solution.get(i));
-                    solution.set(i, solution.get(ss - i));
-                    solution.set(ss - i, temp[i]);
-                }*/
-
 
                 // solution weight
-                solutionWeight = disTo[(int)this.target];
+                //solutionWeight = disTo[(int)this.target];
+                solutionWeight = disTo.get(this.target);
 
                 // outcome
                 outcome = SolverOutcome.SOLVED;
@@ -108,6 +99,7 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
                     timer = sw.elapsedTime();
                 }
             }
+            this.explored += 1;
         }
         // do some decision
         if (sw.elapsedTime() >= this.timeout) {
@@ -122,17 +114,22 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
 
     private void relax(Vertex v, AStarGraph<Vertex> G) {
         for (WeightedEdge<Vertex> e : G.neighbors(v)) {
-            this.explored += 1;
             Vertex p = e.from();
             Vertex q = e.to();
             double w = e.weight();
             //edgeTo[(int)q] = (int)p;
             //edgeTo.add((int)q, p);
-            edgeTo.put(q, p);
-            if (disTo[(int)p] + w < disTo[(int)q]) {
+            //edgeTo.put(q, p);  if set this move will repeat same edge
+            /*if (disTo[(int)p] + w < disTo[(int)q] && !edgeTo.containsKey(q)) {
                 disTo[(int)q] = disTo[(int)p] + (int)w;
+                edgeTo.put(q, p);
+            }*/
+            if (!disTo.containsKey(q) || disTo.get(p) + w < disTo.get(q)) {
+                disTo.put(q, disTo.get(p) + w);
+                edgeTo.put(q, p);
             }
-            double prio = disTo[(int)p] + G.estimatedDistanceToGoal(q, this.target);
+            //double prio = disTo[(int)p] + G.estimatedDistanceToGoal(q, this.target);
+            double prio = disTo.get(p) + G.estimatedDistanceToGoal(q, this.target);
             if (Fringe.contains(q)) {
                 Fringe.changePriority(q, prio);
             } else {
