8e1e929d437923af65061138e217ebc49b217164
Roy Lee <Li-jinquan@outlook.com>
2019-04-18 15:39:18 +0800

    done git add .! passed the local test

--- hw4/bearmaps/hw4/AStarSolver.java
+++ hw4/bearmaps/hw4/AStarSolver.java
@@ -4,6 +4,8 @@ import bearmaps.proj2ab.DoubleMapPQ;
 import edu.princeton.cs.algs4.Stopwatch;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
@@ -18,8 +20,10 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
     private SolverOutcome outcome;
     private DoubleMapPQ Fringe;
     private int[] disTo;
+    //private LinkedList<Vertex> disTo;
     //private int[] edgeTo;
-    private ArrayList<Vertex> edgeTo;
+    //private ArrayList<Vertex> edgeTo;
+    private HashMap<Vertex, Vertex> edgeTo;
     private Stopwatch sw;
 
     // Constructor which finds the solution, computing everything
@@ -30,15 +34,20 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
         this.timeout = timeout;
 
         disTo = new int[(int)end + 1];
+        //disTo = new LinkedList<>();
         //edgeTo = new int[(int)end + 1];
-        edgeTo = new ArrayList<>((int)end + 1);
+        //edgeTo = new ArrayList<>((int)end + 1);
+        edgeTo = new HashMap<>();
 
         disTo[0] = 0;
+        //disTo.add(source);
         for (int i = 1; i < disTo.length; i++) {
             disTo[i] = 99999999;
         }
         // initialize solution (empty list)
-        solution = new ArrayList<>();
+        //solution = new ArrayList<>();
+        solution = new LinkedList<>();
+
 
         // compute start from here!!!
         aStar(input, start);
@@ -49,26 +58,49 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
         Fringe = new DoubleMapPQ();
         int pior = disTo[(int)s] + (int)G.estimatedDistanceToGoal(s, this.target);
         Fringe.add(s, pior);
-        while (Fringe.size() != 0) {
+
+        while (Fringe.size() != 0 && sw.elapsedTime() < timeout) {
             Vertex v = (Vertex) Fringe.removeSmallest();
-            if (v == target) {
+            if (v == this.target) {
 
                 /*while (edgeTo[(int)v] != (int)s) {
                     solution.add(edgeTo[(int)v]);
                 }*/
-                // solution list
-                while (edgeTo.get((int)v) != s) {
-                    solution.add(edgeTo.get((int)v));
+                /*while (edgeTo.get(v) != s) {
+                    solution.add(edgeTo.get(v));
+                    v = edgeTo.get(v);
                 }
-                solution.add(s);
+                solution.add(s);*/
+                // solution list
+                while (v != null) {
+                    ((LinkedList<Vertex>) solution).addFirst(v);
+                    //solution.add(v);
+                    v = edgeTo.get(v);
+                }
+                // reverse
+                /*int ss = solution.size();
+                List<Vertex> temp = new;
+                //int[] temp = new int[ss];
+                /*for (int i = 0; i < temp.length; i++) {
+                    temp[i] = (int) solution.get(ss - i);
+                }
+                for (int i = 0; i < solution.size() / 2; i++) {
+                    //temp[i] = solution.get(i);
+                    temp.add(solution.get(i));
+                    solution.set(i, solution.get(ss - i));
+                    solution.set(ss - i, temp[i]);
+                }*/
+
 
                 // solution weight
-                solutionWeight = disTo[(int)v];
+                solutionWeight = disTo[(int)this.target];
 
                 // outcome
                 outcome = SolverOutcome.SOLVED;
 
                 timer = sw.elapsedTime();
+
+                break;
             } else {
                 relax(v, G);
                 if (sw.elapsedTime() >= this.timeout) {
@@ -77,9 +109,15 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
                 }
             }
         }
-        if (outcome != SolverOutcome.SOLVED || outcome != SolverOutcome.TIMEOUT) {
+        // do some decision
+        if (sw.elapsedTime() >= this.timeout) {
+            outcome= SolverOutcome.TIMEOUT;
+        } else if (solution.size() == 0) {
             outcome = SolverOutcome.UNSOLVABLE;
         }
+        /*if (outcome != SolverOutcome.SOLVED || outcome != SolverOutcome.TIMEOUT) {
+            outcome = SolverOutcome.UNSOLVABLE;
+        }*/
     }
 
     private void relax(Vertex v, AStarGraph<Vertex> G) {
@@ -89,7 +127,8 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
             Vertex q = e.to();
             double w = e.weight();
             //edgeTo[(int)q] = (int)p;
-            edgeTo.add((int)q, p);
+            //edgeTo.add((int)q, p);
+            edgeTo.put(q, p);
             if (disTo[(int)p] + w < disTo[(int)q]) {
                 disTo[(int)q] = disTo[(int)p] + (int)w;
             }
@@ -98,9 +137,6 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
                 Fringe.changePriority(q, prio);
             } else {
                 Fringe.add(q, prio);
-            }
-            if (sw.elapsedTime() >= this.timeout) {
-                break;
             }
         }
     }
